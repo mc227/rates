f467ae342cfe8e3ad51e592905dc714e113da120
mc227 <markcostales.mark@gmail.com>
2023-02-25 23:28:58 -0500

    changed the keys in the api

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5500"}})
 
 data = {
-    'x': 50,
-    'y': 50,
-    'z': 50
+    'eth0': 50,
+    'eth1': 50,
+    'eth2': 50
 }
 
 def update_data():
@@ -23,9 +23,9 @@ def update_data():
     global data
     while True:
         # Update the values of the data object with random values
-        data['x'] = random.randint(0, 100)
-        data['y'] = random.randint(0, 100)
-        data['z'] = random.randint(0, 100)
+        data['eth0'] = random.randint(0, 100)
+        data['eth1'] = random.randint(0, 100)
+        data['eth2'] = random.randint(0, 100)
         time.sleep(10)  # Wait for 1 seconds before updating again
 
 # Start the timer to update the data object
